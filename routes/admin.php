e95e8590c4f675fba7682ebd52a8861449982a62
Gregorius000 <agunggregorius253@gmail.com>
2024-05-12 10:14:24 +0700

    update

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/beranda', [HomeController::class, 'beranda']);
+//
+Route::get('/beranda', [UsersController::class, 'beranda']);
