1b4d7a9b59e34d088021b51f76516665fd58ee18
Gregorius000 <agunggregorius253@gmail.com>
2024-05-15 18:15:04 +0700

    up: transation

--- database/seeders/DetailTransactionSeeder.php
+++ database/seeders/DetailTransactionSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\DetailTransaction;
 use Illuminate\Database\Seeder;
 
 class DetailTransactionSeeder extends Seeder
@@ -12,6 +12,9 @@ class DetailTransactionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DetailTransaction::create([
+            'product_id' => 4,
+            'transaction_id' => 2,
+        ]);
     }
 }
--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -2,16 +2,38 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TransactionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('transactions')->insert([
+            [
+                'first_name' => 'Gregorius',
+                'last_name' => 'Agung',
+                'address' => 'Bandungrejosari',
+                'city' => 'Sukun',
+                'state' => 'Jawa Timur',
+                'country' => 'Indonesia',
+                'zip_code' => '65148',
+                'email_address' => 'agunggregorius845@gmail.com',
+                'phone_number' => '085608674014',
+                'total_checkout' => 10,
+                'status' => 'completed',
+                'user_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+                'shipping_cost' => 0,
+                'shipping_detail' => 'Detail pengiriman contoh',
+            ],
+            // 
+        ]);
     }
 }
